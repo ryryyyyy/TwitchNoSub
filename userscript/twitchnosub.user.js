5638fc4dc7379327fefbe2b5488c9209e6bba49a
ryryyyyy <benahmidirywane@gmail.com>
2025-01-15 16:04:27 +0100

    Update twitchnosub.user.js

--- userscript/twitchnosub.user.js
+++ userscript/twitchnosub.user.js
@@ -33,22 +33,21 @@
     const oldWorker = window.Worker;
 
     window.Worker = class Worker extends oldWorker {
-        constructor() {
-            super(URL.createObjectURL(new Blob(["importScripts('https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub/src/patch_amazonworker.js', 'https://cdn.jsdelivr.net/npm/amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js');"])));
+       constructor(twitchBlobUrl) {
+           super(twitchBlobUrl);
 
-            if (!isVariantA) {
-                this.addEventListener("message", (event) => {
-                    const data = event.data;
+           this.addEventListener("message", (event) => {
+               const data = event.data;
 
-                    if (data.id == 1 && data.type == 1) {
-                        const newData = event.data;
+               if ((data.id == 1 || isVariantA) && data.type == 1) {
+                  const newData = event.data;
 
-                        newData.arg = [data.arg];
+                  newData.arg = [data.arg];
 
-                        this.postMessage(newData);
-                    }
-                });
-            }
-        }
+                  this.postMessage(newData);
+               }
+           });
+       }
+  }
     }
 })();
